5f8ea61325698649fd41a0e97e799c6fa11b3283
Jesse <jesseroyc@gmail.com>
2018-03-18 20:07:30 +0000

    added a test module and some dependencies

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require('path'); //builds path from compile to inbox.sol
 const fs = require('fs');     //makes this project more cross-platform
 const solc = require('solc');
 
-const inboxPath = path.resolve(_dirname, 'contracts', 'Inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 console.log(solc.compile(source, 1));
---
+++ test/inbox.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+const ganache = require('ganache-cli'); //local test-net
+const Web3 = require('web3'); //note that the variable is not camel case, its a class
+
+
+
